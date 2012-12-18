27d4d254e8eb353f22b0ea9441b8464ea63c4478
Michal Bojanowski <michal2992@gmail.com>
2012-12-18 19:02:03 +0100

    Added description to package R file

--- R/isnar-package.R
+++ R/isnar-package.R
@@ -1,4 +1,7 @@
-#' isnar
+#' Introduction to Social Network Analysis with R
+#'
+#' Functions and data accompanying the book "Introduction to Social Network
+#' Analysis with R"
 #' 
 #' @name isnar
 #' @docType package
