2744f38bc82652d60aadd371464f2a08ad7e4976
Michal Bojanowski <michal2992@gmail.com>
2012-12-19 02:13:52 +0100

    Added Catania data from asnap

---
+++ data/Catania.R
@@ -0,0 +1,8 @@
+
+Catania <- matrix( nrow=4, ncol=4, byrow=TRUE,
+    c(  .258, .016, .035, .013,
+        .012, .157, .058, .019,
+        .013, .023, .306, .035,
+        .005, .007, .024, .016 ),
+    dimnames = list( man = c("black", "hispanic", "white", "other"),
+        woman=c("black", "hispanic", "white", "other")) )
