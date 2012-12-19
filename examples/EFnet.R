68a7db5b8fa1d5b18399631e4faac08cb95399d3
Michal Bojanowski <michal2992@gmail.com>
2012-12-19 01:39:00 +0100

    Added EFnet data from asnap

---
+++ examples/EFnet.R
@@ -0,0 +1,7 @@
+data(EFnet)
+
+set.seed(2992)
+plot(EFnet, layout=layout.fruchterman.reingold,
+    vertex.color=V(EFnet)$type+1, vertex.label.family="",
+    sub="Source: Echenique & Fryer (2006)",
+    main="Neighborhood racial segregation\n in a fictional city" )
