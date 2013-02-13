a8ee059c3fb5e459610c2d6801a97c62b32de3ce
Michal Bojanowski <michal2992@gmail.com>
2013-02-13 17:14:59 +0100

    Fixed examples of mixingm
    Removed the picture and added undirected example.

--- examples/mixingm.R
+++ examples/mixingm.R
@@ -2,13 +2,10 @@
 net <- graph(c(1,2, 1,3, 2,3,  4,5,  1,4, 1,5, 4,2, 5,3))
 V(net)$type <- c(1,1,1, 2,2)
 
-# plot it
-plot(net, vertex.color=V(net)$type + 1, vertex.label.family="",
-  layout=layout.fruchterman.reingold)
-
 mixingm(net, "type")
 mixingm(net, "type", full=TRUE)
 
 # as undirected
 mixingm( as.undirected(net), "type")
+mixingm(net, "type")
 mixingm(net, "type", full=TRUE)
