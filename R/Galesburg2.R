be675eaccd29fa2f3181f66ccf29f33dc8108f63
Michal Bojanowski <michal2992@gmail.com>
2013-02-13 17:40:40 +0100

    Added Galesburg2 data

---
+++ R/Galesburg2.R
@@ -0,0 +1,29 @@
+#' Columbia University Drug Study data 2
+#' 
+#' Data from Columbia University Drug Study on diffusion of medical innovation
+#' in the form of a new drug (gammanym). Its a network of 31 medical doctors
+#' connected with friendship or discussion links. Vertex attribute "adoption"
+#' specifies time of first prescription of the new drug.
+#'
+#' @docType data
+#' @name Galesburg2
+#'
+#' @format
+#' Directed network (class 'igraph') with edge attributes:
+#' \describe{
+#' \item{discussion}{logical, discussion link}
+#' \item{friendship}{logical, friendship nomination}
+#' }
+#' and vertex attributes:
+#' \describe{
+#' \item{adoption}{numeric, time of adoption}
+#' }
+#'
+#' @source
+#' Pajek datasets
+#' \url{http://vlado.fmf.uni-lj.si/pub/networks/data/esna/Galesburg2.htm}
+#'
+#' @references
+#' Coleman, J.S., E. Katz, H. Menzel (1966) "Medical Innovation. A Diffusion
+#' Study", Indianapolis: Bobbs-Merrill
+NULL
