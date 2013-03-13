45bacf166a00c7b50a278cd3f9df3734b8875a0d
Michal Bojanowski <michal2992@gmail.com>
2013-03-13 01:36:13 +0100

    Package man page

--- R/isnar-package.R
+++ R/isnar-package.R
@@ -3,7 +3,7 @@
 #' Functions and data accompanying the book "Introduction to Social Network
 #' Analysis with R"
 #'
-#' Among other things, it includes
+#' Among other things, it includes a lot of things.
 #' 
 #' @name isnar
 #' @docType package
