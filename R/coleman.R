a1e01e041b0e182b02445130e679821126a9707e
Michal Bojanowski <michal2992@gmail.com>
2013-03-14 16:30:14 +0100

    Coleman only for directed networks

--- R/coleman.R
+++ R/coleman.R
@@ -1,8 +1,6 @@
 #' Coleman's index of network segregation
 #'
-#' Colemans's measure for segregation in networks. Designed in principle for
-#' directed networks, but no formal obstacles for using it in undirected
-#' context.
+#' Colemans's measure for segregation in directed networks.
 #'
 #' @param g object of class "igraph", the network
 #'
@@ -19,33 +17,24 @@
 #' @family segregation measures
 coleman <- function(g, vattr)
 {
-    stopifnot(inherits(g, "igraph"))
-    a <- get.vertex.attribute(g, vattr)
-    m <- mixingm(g, vattr)
-    # expected number of WG choices
-    # NOTE should the degree be "out" for the directed networks?
-    if( is.directed(g) )
-    {
-        degsums <- tapply( degree(g, mode="out"), a, sum )
-    } else {
-        degsums <- tapply( degree(g, mode="all"), a, sum )
-    }
-    gsize <- table(a)
-    ewg <- degsums * (gsize - 1) / (vcount(g) - 1)
-    # rval
-    if( is.directed(g) )
-    {
-        r <- (diag(m) - ewg) / (degsums - ewg)
-        i <- diag(m) <= ewg
-        repl <- (diag(m) - ewg) / ewg
-        r[i] <- repl[i]
-    } else
-    {
-        warning("coleman's index for undirected networks is experimental")
-        r <- (diag(m)*2 - ewg) / (degsums - ewg)
-        i <- diag(m)*2 <= ewg 
-        repl <- (diag(m)*2 - ewg) / ewg
-        r[i] <- repl[i]
-    }
-    structure(as.numeric(r), names=names(gsize))
+  stopifnot(inherits(g, "igraph"))
+  stopifnot(is.directed(g))
+  a <- get.vertex.attribute(g, vattr)
+  m <- mixingm(g, vattr)
+  # expected number of WG choices
+  # NOTE should the degree be "out" for the directed networks?
+  if( is.directed(g) )
+  {
+    degsums <- tapply( degree(g, mode="out"), a, sum )
+  } else {
+    degsums <- tapply( degree(g, mode="all"), a, sum )
+  }
+  gsize <- table(a)
+  ewg <- degsums * (gsize - 1) / (vcount(g) - 1)
+  # rval
+  r <- (diag(m) - ewg) / (degsums - ewg)
+  i <- diag(m) <= ewg
+  repl <- (diag(m) - ewg) / ewg
+  r[i] <- repl[i]
+  structure(as.numeric(r), names=names(gsize))
 }
