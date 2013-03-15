11986f4b5c3b2c95146845d88684a447ee427596
Michal Bojanowski <michal2992@gmail.com>
2013-03-15 17:47:31 +0100

    Added test coleman for hist 1958 example matrix

--- inst/tests/test-coleman.R
+++ inst/tests/test-coleman.R
@@ -6,3 +6,16 @@ test_that("Coleman index works for Hnatiuk data", {
           V(g)$a <- c(1,1, 2,2, 3,3,3,3,3,3)
           coleman(g, "a")
 } )
+
+test_that("Coleman index gives correct results for his example data",
+          {
+            # coleman's matrix
+            mat <- matrix(c(45, 20, 15, 20), 2, 2)
+            # rebuild mixing matrix given group sizes
+            mmat <- as.mixingm(mat, full=TRUE, directed=TRUE, gsizes=c(60, 40))
+            r <- coleman(mmat)
+            # NOTE In Colemans paper (1958) he uses approximation in computing
+            # expected number of ties within group 'i'. The function is exact.
+            # Original results were: boys=0.375 and girls=0.167
+            expect_equal(r, c(0.38125, 0.175))
+          } )
