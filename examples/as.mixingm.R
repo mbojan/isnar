2cf0791f29c4e3bff3d2d4bfc4b22001acf2f5eb
Michal Bojanowski <michal2992@gmail.com>
2013-03-19 15:35:37 +0100

    examples for as.mixingm

--- examples/as.mixingm.R
+++ examples/as.mixingm.R
@@ -2,10 +2,10 @@
 net <- graph(c(1,2, 1,3, 2,3,  4,5,  1,4, 1,5, 4,2, 5,3))
 V(net)$type <- c(1,1,1, 2,2)
 
-mixingm(net, "type")
-mixingm(net, "type", full=TRUE)
+as.mixingm(net, "type")
+as.mixingm(net, "type", full=TRUE)
 
 # as undirected
-mixingm( as.undirected(net), "type")
-mixingm(net, "type")
-mixingm(net, "type", full=TRUE)
+as.mixingm( as.undirected(net), "type")
+as.mixingm(net, "type")
+as.mixingm(net, "type", full=TRUE)
