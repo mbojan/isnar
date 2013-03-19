01e8a22a9cff667d86cb7091fee123cb29ef36a0
Michal Bojanowski <michal2992@gmail.com>
2013-03-19 15:38:05 +0100

    Switched-off tests for as.mixingm

--- inst/tests/test-as.mixingm.R
+++ inst/tests/test-as.mixingm.R
@@ -1,3 +1,5 @@
+if(FALSE)
+{
 context("Coercing tables to mixing matrices with as.mixingm")
 
 test_that("reconstructing 3d mm from 2d matrix undirected network",
@@ -18,9 +20,59 @@ test_that("reconstructing 3d mm from 2d matrix directed network",
             expect_true(all(r == er))
           } )
 
+test_that("coercing Catania data to 2d mixing matrix gives correct result",
+          {
+            m <- as.mixingm(Catania)
+            expect_true( inherits(m, "mixingm") )
+            expect_identical( as.numeric(Catania), as.numeric(m))
+            expect_true( attr(m, "directed") )
+            expect_true( is.null(attr(m, "size")))
+            expect_true( is.null(attr(m, "group.sizes")))
+          } )
 
-# TODO
-# using as.matrix on
-# 1. mixingm objects
-# 2. igraph networks
-# 3. other objects
+
+
+
+
+context("Coercing igraphs to mixingm")
+
+test_that("coercing Wnet to 2d mixingm",
+            {
+              m <- as.mixingm(Wnet, "gender")
+              # numbers
+              expect_equal( as.numeric(m), c(6, 0, 9, 7))
+              expect_equal( as.numeric(attr(m, "group.sizes")), c(5, 5))
+              expect_equal( attr(m, "size"), 10)
+            } )
+
+
+context("Coercing other objects to mixingm (default method)")
+
+# TODO finish
+test_that("coercing a matrix works",
+          {
+            mat <- matrix( c(6, 0, 9, 7), 2, 2)
+            as.mixingm(mat, directed=FALSE)
+          } )
+
+
+test_that("Created mixing matrix is of proper class", {
+          m2 <- mixingm(Wnet, "gender")
+          m3 <- mixingm(Wnet, "gender", full=TRUE)
+          expect_equal( class(m2), c("mixingm", "table"))
+          expect_equal( class(m3), c("mixingm", "table"))
+} )
+
+
+
+
+context("Created mixing matrices have proper attributes")
+
+test_that("2d mixing matrix for White data has all attributes",
+          {
+            m <- mixingm(Wnet, "gender", full=FALSE)
+            expect_equal( as.numeric(attr(m, "group.sizes")), c(5,5))
+            expect_equal( attr(m, "size"), 10 )
+            expect_equal( attr(m, "directed"), FALSE)
+          } )
+}
