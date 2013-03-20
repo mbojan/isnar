f86335d9a0d757b77145ed1d0e9628e1af3ebaf7
Michal Bojanowski <michal2992@gmail.com>
2013-03-20 14:51:59 +0100

    Updated Odds Ratio for Within-Group ties
    Renamed from 'moody' to 'orwg' and it is now generic with methods for
    mixingm and igraph.

---
+++ R/orwg.R
@@ -0,0 +1,57 @@
+#' Odds ratio of existence of within-group ties
+#'
+#' Odds ratio for connected, as opposed to disconnected, dyads depending
+#' whether it is between- or within-group, i.e. how much more likely the dyad
+#' will be connected if it is within-group.
+#'
+#' The measure takes values, like all odds ratios, from (0; Inf).
+#'
+#' @param object R object, see Details for available methods
+#'
+#' @param ... other arguments passed to/from other methods
+#'
+#' @return Numeric value of the measure.
+#'
+#' @references
+#' Moody, Jim (2001) "Race, school integration, and friendship segregation in
+#' America", American Journal of Sociology, 107(3):679--377
+#'
+#' @export
+#' @example examples/orwg.R
+#' @family segregation measures
+orwg <- function(object, ...) UseMethod("orwg")
+
+
+#' @details
+#' Method for mixing matrices.
+#'
+#' @method orwg mixingm
+#' @export
+#' @rdname orwg
+orwg.mixingm <- function(object, ...)
+{
+  # needs full mixing matrix
+  stopifnot(length(dim(object)) == 3)
+  # compute number of b-g and w-g dyads in both layers
+  z <- apply(object, 3, function(x)
+             c(between= sum(x) - sum(diag(x)),
+               within=sum(diag(x)))
+  )
+  offdiag <- z[ col(z) != row(z) ]
+  prod(diag(z)) / prod(offdiag)
+}
+
+#' @details
+#' Method for igraphs
+#'
+#' @param vattr character scalar or any vector, name of the vertex attribute or
+#' the attribute itself (as a vector)
+#'
+#' @method orwg igraph
+#' @export
+#' @rdname orwg
+orwg.igraph <- function(object, vattr, ...)
+{
+  m <- as.mixingm(object, vattr=vattr, full=TRUE)
+  orwg(m, ...)
+}
