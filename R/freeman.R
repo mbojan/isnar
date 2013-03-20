12aa430295827a5d5a1c2bd69d99498bf40fac7e
Michal Bojanowski <michal2992@gmail.com>
2013-03-20 14:04:38 +0100

    Updated freeman and its methods

--- R/freeman.R
+++ R/freeman.R
@@ -1,21 +1,16 @@
 #' Generalized Freeman's segregation index
 #'
-#' Calculate generalized Freeman's segregation index for undirected netoworks
-#' with arbitrary number of groups.
+#' Calculate Freeman's segregation index for undirected netoworks with
+#' arbitrary number of groups.
 #'
 #' Freeman's segregation index (Freeman, 1978) is designed to capture the
-#' extent to which the defined types of vertices tend to have more edges with
-#' vertices from the same type than with other types (segregation).  Formally,
-#' the index compares the number of edges in the specified graph that connect
-#' vertices of different type to the number of ``inter-type'' edges that would
-#' be expected under pure random graph. High values indicate that there one
-#' would expect much more inter-type edges if they are to be created without
-#' considering the type of vertices, suggesting high segregation. Low values
-#' indicate that lower segregation. Especially, the index is 0 for the full
-#' graph (all possible edges are present).
+#' extent to which the defined groups of vertices tend to have more edges with
+#' vertices from the same group than with other groups.  Formally, the index
+#' compares the observed number of between-group ties with the number of
+#' between-group ties that would be expected if ties would be created randomly.
 #'
-#' The index has some discontinuity as there are graph and type configurations
-#' that are characterized by the higher number of inter-type edges that is
+#' The index has some discontinuity as there are network and group configurations
+#' that are characterized by the higher number of between-group ties that is
 #' expected under a random graph. The index is truncates these situations and
 #' takes a value of 0.
 #'
@@ -24,8 +19,6 @@
 #' affects the way in which the expected number of inter-type edges under pure
 #' random graph is calculated.
 #'
-#' About the function itself:
-#'
 #' The function internally calculates the frequency of types of vertices in the
 #' supplied attributer \code{vattr}. However, it is possible to override this
 #' by specifying ``true'' type distribution with the \code{dis} argument. It is
@@ -33,18 +26,15 @@
 #' frequencies of types of vertices. This may be especially usefull when
 #' dealing with large graphs with larger number of isolates.
 #'
-#' @param g object of class "igraph" representing the network
+#' @param object R object, see Details for available methods
 #'
-#' @param vattr name of vertex attribute in \code{g}
-#'
-#' @param dis numeric, optional true distribution of types, see Details
-#'
-#' @param more logical, should some more output be returned
+#' @param ... other arguments passed to/from other methods
 #'
 #' @return 
 #' The value of the Freeman's index.
 #'
-#' If \code{more} is \code{TRUE} some intermediate results returned in a list.
+#' If \code{more} is \code{TRUE}, some intermediate results are returned in a
+#' list.
 #'
 #' @references
 #' Freeman, Linton C. (1978) Segregation in Social Networks, \emph{Sociological
@@ -54,28 +44,59 @@
 #' @keywords graphs
 #' @export
 #' @family segregation measures
-freeman <- function(g, vattr, dis=NULL, more=FALSE)
+freeman <- function(object, ...) UseMethod("freeman")
+
+
+#' @details
+#' Method for mixing matrices
+#'
+#' @param dis numeric, optional true distribution of types, see Details
+#'
+#' @param more logical, should some more output be returned
+#'
+#' @method freeman mixingm
+#' @rdname freeman
+freeman.mixingm <- function(object, dis=attr(object, "gsizes"), more=FALSE, ...)
 {
-    stopifnot(inherits(g, "igraph"))
-    stopifnot(!is.directed(g))
-    n <- ecount(g)
-    m <- mixingm(g, vattr)
-    # number of cross-group ties
-    cct <- sum(m) - sum(diag(m))
-    # group distribution
-    if(is.null(dis))
-        btab <- table( get.vertex.attribute(g, vattr))
-    else
-        btab <- dis
-    m <- sum(btab)
-    ecct <- (n * (sum(btab)^2 - sum(btab^2))) / (m * (m - 1))
-    s <- ecct - cct
-    if(s < 0)  s <- 0
-    if(more)
-        return(list(ecct = ecct, cct = cct, n = n, m = m, btab = btab))
-    else
-        return(s/ecct)
+  stopifnot(!attr(object, "directed"))
+  # need group sizes
+  stopifnot(!is.null(attr(object, "gsizes")))
+  # take contact layer
+  if( length(dim(object)) == 3 )
+    m <- object[,,2]
+  else m <- object
+  # number of cross-group ties
+  cct <- sum(m) - sum(diag(m))
+  # group distribution
+  btab <- dis
+  n <- sum(dis)
+  m <- sum(btab)
+  ecct <- (n * (sum(btab)^2 - sum(btab^2))) / (m * (m - 1))
+  s <- ecct - cct
+  if(s < 0)  s <- 0
+  if(more)
+      return(list(ecct = ecct, cct = cct, n = n, m = m, btab = btab))
+  else
+      return(s/ecct)
 }
 
-   
 
+
+
+
+#' @details
+#' Method for "igraph"s
+#'
+#' @param vattr character scalar or any vector of length equal to
+#' \code{vcount(object)}, name of the vertex attribute in \code{object}
+#' designating the groups or a vector with the attribute itself
+#'
+#' @method freeman igraph
+#' @rdname freeman
+freeman.igraph <- function(object, vattr, dis=NULL, ...)
+{
+    stopifnot(!is.directed(object))
+    m <- as.mixingm(object, vattr, full=TRUE)
+    if(is.null(dis)) dis <- attr(m, "gsizes")
+    freeman(m, dis=dis, ...)
+}
