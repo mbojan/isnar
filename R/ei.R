29eafbe5b026511eae2600911ea47ad3391857aa
Michal Bojanowski <michal2992@gmail.com>
2013-03-20 14:39:09 +0100

    Converted EI to generic
    with methods for mixingm and igraph

--- R/ei.R
+++ R/ei.R
@@ -4,24 +4,46 @@
 #' prevalence of between- and within-group ties. From that perspective it can
 #' be interpreted as a measure of network segregation.
 #'
-#' @param g object of class "igraph", the network
+#' @param object R object, see Details for available methods
 #'
-#' @param vattr  character scalar or vector of length equal to the size of
-#' \code{g}, vertex attribute for which mixing matrix is to be computed
+#' @param ... other arguments passed to/from other methods
 #'
-#' @param ... other arguments, currently ignored
-#'
-#' @return Numerical value of the E-I index for network \code{g}.
+#' @return Numerical value of the E-I index.
 #'
 #' @example examples/ei.R
 #' @export
 #' @family segregation measures
-ei <- function(g, vattr, ...)
+ei <- function(object, ...) UseMethod("ei")
+
+#' @details
+#' Method for mixing matrices
+#' @method ei mixingm
+#' @rdname ei
+#' @export
+ei.mixingm <- function(object, ...)
 {
-  m <- mixingm(g, vattr=vattr)
+  # extract contact layer
+  if(length(dim(object)) == 3)
+    m <- object[,,2]
+  else m <- object
   p <- m / sum(m)
   pinternal <- sum(diag(p))
   diag(p) <- 0
   pexternal <- sum(p)
   return( pexternal - pinternal )
+}
+
+#' @details
+#' Method for igraphs
+#'
+#' @param vattr character scalar or vector of length equal to the size of
+#' \code{object}, vertex attribute for which mixing matrix is to be computed
+#'
+#' @method ei igraph
+#' @rdname ei
+#' @export
+ei.igraph <- function(object, vattr, ...)
+{
+  m <- as.mixingm(object, vattr=vattr)
+  ei(m, ...)
 }
