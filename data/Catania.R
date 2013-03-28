d2d75f2a39f78f4d4f3e87955072458508d5b757
Michal Bojanowski <michal2992@gmail.com>
2013-03-28 16:06:40 +0100

    Catania data as counts, not frequencies

--- data/Catania.R
+++ data/Catania.R
@@ -1,8 +1,8 @@
 
 Catania <- matrix( nrow=4, ncol=4, byrow=TRUE,
-    c(  .258, .016, .035, .013,
-        .012, .157, .058, .019,
-        .013, .023, .306, .035,
-        .005, .007, .024, .016 ),
+    c(506 , 32  , 69  , 26 , 
+      23  , 308 , 114 , 38 , 
+      26  , 46  , 599 , 68 , 
+      10  , 14  , 47  , 32),
     dimnames = list( man = c("black", "hispanic", "white", "other"),
         woman=c("black", "hispanic", "white", "other")) )
