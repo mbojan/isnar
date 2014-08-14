65597a42836642a3c839cda76daec6fd66b76f53
Michal Bojanowski <michal2992@gmail.com>
2014-08-14 17:41:34 +0200

    Commented old code, promoted new mixingm

--- R/mixingm.R
+++ R/mixingm.R
@@ -29,80 +29,12 @@
 #' folded onto the upper triangle (entries in lower triangle are 0).
 #'
 #' @export
-mixingm <- function(mat, gsizes=NULL, directed=TRUE, loops=FALSE, size=NULL,
-                    foldit=TRUE)
-{
-  stopifnot(is.array(mat))
-  # check for proper dimensionality of 'mat'
-  dims <- dim(mat)
-  stopifnot( length(dims) %in% 2:3 )
-  stopifnot( dims[1] == dims[2] )
-  if(length(dims) == 3) stopifnot(dims[3] == 2)
-  d3 <- length(dims) == 3
-  # other arguments
-  if(!is.null(gsizes)) stopifnot( length(gsizes) == dims[1] )
-  if(!is.null(gsizes)) size <- sum(gsizes)
-  # TODO check if 'mat' can be mixing matrix
-  # If undirected, fold onto upper triangle
-  if( !directed && foldit )
-  {
-    if(d3)
-    {
-      dims <- dim(mat)
-      mat <- apply(mat, 3, fold, direction="upper")
-      dim(mat) <- dims
-    } else
-    {
-      mat <- fold(mat, direction="upper")
-    }
-  }
-  r <- as.table(mat)
-  structure( r,
-            dimnames=dimnames(r),
-            directed=directed,
-            loops=loops,
-            gsizes=gsizes,
-            size=size,
-            class=c("mixingm", "table")
-            )
-}
+mixingm <- function(g, ...) UseMethod("mixingm2")
 
-
-#' @param x object of class "mixingm"
-#'
-#' @param ... other arguments passed to/from other methods
-#'
-#' @method print mixingm
+#' @method mixingm igraph
 #' @export
 #' @rdname mixingm
-print.mixingm <- function(x, ...)
-{
-  z <- structure(as.numeric(x), dim=dim(x), dimnames=dimnames(x))
-  cat("Mixing matrix\n")
-  cat("Is directed:", attr(x, "directed"), "\n")
-  cat("Network size:", attr(x, "size"), "\n")
-  tab <- attr(x, "gsizes")
-  cat("Group sizes:\n")
-  print(tab)
-  print(z)
-}
-
-
-
-
-
-
-
-
-
-
-
-#============================================================================ 
-# Mixing matrix, ver2
-# Creating mixing matrix given two vertex attributes
-mixingm2 <- function(g, ...) UseMethod("mixingm2")
-
-mixingm2.igraph <- function(g, rattr, cattr=rattr, full=FALSE,
+mixingm.igraph <- function(g, rattr, cattr=rattr, full=FALSE,
                             directed = is.directed(g),
                             loops=any(is.loop(g)), ...)
 {
@@ -136,3 +68,66 @@ mixingm2.igraph <- function(g, rattr, cattr=rattr, full=FALSE,
     return(con)
   }
 }
+
+
+
+
+
+
+
+
+
+
+#============================================================================ 
+# old code
+# 
+# mixingm <- function(mat, gsizes=NULL, directed=TRUE, loops=FALSE, size=NULL,
+#                     foldit=TRUE)
+# {
+#   stopifnot(is.array(mat))
+#   # check for proper dimensionality of 'mat'
+#   dims <- dim(mat)
+#   stopifnot( length(dims) %in% 2:3 )
+#   stopifnot( dims[1] == dims[2] )
+#   if(length(dims) == 3) stopifnot(dims[3] == 2)
+#   d3 <- length(dims) == 3
+#   # other arguments
+#   if(!is.null(gsizes)) stopifnot( length(gsizes) == dims[1] )
+#   if(!is.null(gsizes)) size <- sum(gsizes)
+#   # TODO check if 'mat' can be mixing matrix
+#   # If undirected, fold onto upper triangle
+#   if( !directed && foldit )
+#   {
+#     if(d3)
+#     {
+#       dims <- dim(mat)
+#       mat <- apply(mat, 3, fold, direction="upper")
+#       dim(mat) <- dims
+#     } else
+#     {
+#       mat <- fold(mat, direction="upper")
+#     }
+#   }
+#   r <- as.table(mat)
+#   structure( r,
+#             dimnames=dimnames(r),
+#             directed=directed,
+#             loops=loops,
+#             gsizes=gsizes,
+#             size=size,
+#             class=c("mixingm", "table")
+#             )
+# }
+# 
+# 
+# print.mixingm <- function(x, ...)
+# {
+#   z <- structure(as.numeric(x), dim=dim(x), dimnames=dimnames(x))
+#   cat("Mixing matrix\n")
+#   cat("Is directed:", attr(x, "directed"), "\n")
+#   cat("Network size:", attr(x, "size"), "\n")
+#   tab <- attr(x, "gsizes")
+#   cat("Group sizes:\n")
+#   print(tab)
+#   print(z)
+# }
