cf900972eba3e8c086b57af46795c615860c50d0
Michal Bojanowski <michal2992@gmail.com>
2015-02-15 16:04:37 +0100

    Added Input_output data

---
+++ R/Input_output.R
@@ -0,0 +1,19 @@
+#' Input-output table for U.S. economy
+#'
+#' Based on data from TODO
+#'
+#'
+#'
+#' @docType data
+#' @name Input_output
+#'
+#' @format
+#' 22-by-22 numeric matrix with dimnames.
+#'
+#' @source
+#' TODO
+#'
+#' @references
+#'
+#' TODO: Bojanowski () "Industrial structure and interfirm collaboration".
+NULL
