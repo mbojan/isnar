443e2c1fec5d56c3fa759ee5db2e1f01d60a0f05
Michal Bojanowski <michal2992@gmail.com>
2015-10-27 15:13:37 +0100

    Fixing examples

--- examples/Wnet.R
+++ examples/Wnet.R
@@ -1,8 +1,9 @@
-data(Wnet)
-
-set.seed(2992)
-plot(Wnet, layout=layout.fruchterman.reingold,
-    vertex.color=V(Wnet)$gender + 1,
-    vertex.label=V(Wnet)$name, vertex.label.family="",
-    main="White's (1975) data on kinship networks")
-legend("topleft", col=2:3, legend=c("Woman", "Man"), pch=19)
+if( require(igraph, quietly = TRUE) ) {
+  data(Wnet)
+  set.seed(2992)
+  plot(Wnet, layout=layout.fruchterman.reingold,
+       vertex.color=V(Wnet)$gender + 1,
+       vertex.label=V(Wnet)$name, vertex.label.family="",
+       main="White's (1975) data on kinship networks")
+  legend("topleft", col=2:3, legend=c("Woman", "Man"), pch=19)
+}
--- examples/assort.R
+++ examples/assort.R
@@ -1,16 +1,19 @@
 assort(Wnet, "gender")
 assort(EFnet, "type")
 
-# value of 'assort' for full networks of different sizes
-f <- function(n)
-{
-  gfull <- graph.full(n, directed=FALSE)
-  V(gfull)$type <- rep(1:2, length=vcount(gfull))
-  assort(gfull, "type")
+
+if( require(igraph, quietly = TRUE) ) {
+  # value of 'assort' for full networks of different sizes
+  f <- function(n)
+  {
+    gfull <- graph.full(n, directed=FALSE)
+    V(gfull)$type <- rep(1:2, length=vcount(gfull))
+    assort(gfull, "type")
+  }
+  set.seed(1)
+  x <- sort(sample(5:100, 25) * 2)
+  y <- sapply(x, f)
+  plot(x, y, type="o",
+       xlab="Network size", ylab="Assortativity coefficient",
+       main="Assortativity coef. for full networks of different sizes")
 }
-set.seed(1)
-x <- sort(sample(5:100, 25) * 2)
-y <- sapply(x, f)
-plot(x, y, type="o",
-  xlab="Network size", ylab="Assortativity coefficient",
-  main="Assortativity coef. for full networks of different sizes")
