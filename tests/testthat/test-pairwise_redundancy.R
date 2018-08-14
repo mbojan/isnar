308e62461aac726af1ffe58c9a9764fc0fc77082
Michal Bojanowski <michal2992@gmail.com>
2018-08-14 15:46:01 +0200

    Added test of PR-related functions

---
+++ tests/testthat/test-pairwise_redundancy.R
@@ -0,0 +1,42 @@
+context("Testing pr_invdistance()")
+
+# IBE graph as undirected network
+playnet <- delete_edges(IBE121, E(IBE121)[question != "play"] ) %>%
+  as.undirected()
+# plot(playnet, vertex.label.cex=0.7)
+
+
+test_that("it works silently", {
+  expect_silent(
+    r <- pr_invdistance(playnet)
+  )
+})
+
+
+
+
+
+
+context("Testing pr_sharedp()")
+
+test_that("it works silently", {
+  expect_silent(
+    r <- pr_sharedp(playnet)
+  )
+})
+
+
+
+
+
+
+context("Testing pairwise_redundancy()")
+
+test_that("it works silently", {
+  expect_silent(
+    r <- pairwise_redundancy(playnet)
+  )
+})
+
+
+
